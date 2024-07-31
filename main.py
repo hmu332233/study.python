e62e190434766dc3d73c8038e75e40d84a7c9167
Minung Han <hmu332233@gmail.com>
2024-07-31 14:35:29 +0000

    2.11

--- main.py
+++ main.py
@@ -4,10 +4,25 @@ print("Hello world!")
 # 2.3
 booleanVal = True # False | true, false가 아니라니
 
-
 # 2.4 2.6 2.7
 def sayHello(name):
   print('Hello!', name)
 
 sayHello("Minung")
 # sayHello("Minung", 123) # error
+
+# 2.9
+def sayHelloWithDefault(name = "min"):
+  print('Hello!', name)
+
+sayHelloWithDefault()
+
+# 2.10
+def plus(a, b):
+  return a + b;
+
+print(plus(1, 2))
+
+# 2.11
+name = "Minung"
+print(f"Hello {name}") # f는 format의 의미
